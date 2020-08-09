c2dd7bceafb661838f9e8807f8ea79dabb834387
seemaamalkani <seema.s.malkani.adit@gmail.com>
2020-08-09 15:01:45 +1000

    quiz

---
+++ src/app/question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core' // import component library from angular core
+
+@Component({
+    selector : 'question',
+    template: `this is our question`
+})
+
+export class QuestionComponent {
+
+}
